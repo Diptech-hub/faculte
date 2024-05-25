14c8eb7934c3034e1f5e5760a69cd1ab7c081936
Diptech-hub <oladipupobabayemi@gmail.com>
2024-05-25 11:00:56 +0100

    community carousel

--- src/components/community.tsx
+++ src/components/community.tsx
@@ -1,47 +1,17 @@
+import React, { useState } from "react";
 import azeez from "../assets/azeez.png";
 import "../styles/community.css";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import { FaRegArrowAltCircleLeft } from "react-icons/fa";
+import { FaRegArrowAltCircleRight } from "react-icons/fa";
 
+interface DataItem {
+  img: string;
+  name: string;
+  text: string;
+  review: string;
+}
 
-const Community: React.FC = () => {
-    const settings = {
-        dots: true,
-    fade: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    waitForAnimate: false
-      };
-      
-  return (
-    <div className="communityhead">
-      <div className="community">
-        <p>From the Faculte community</p>
-        
-        <div className="community__body">
-        <Slider {...settings}>
-          {data.map((d) => (
-            <div className="community__body1">
-                <img src={d.img} alt={d.name} />
-              <div className="community__body1content">
-                <p>{d.name}</p>
-                <p className="text">{d.text}</p>
-                {/* <div className=".line"></div> */}
-                <p>{d.review}</p>
-              </div>
-            </div>
-          ))}
-          </Slider>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-const data = [
+const data: DataItem[] = [
   {
     img: azeez,
     name: "Azeez Laughter",
@@ -71,5 +41,56 @@ const data = [
       "“Thanks to Udemy Business, Booz Allen has armed our workforce, specifically its data scientists, with highly relevant and in-demand tech skills that are enabling consultants to stay ahead of big data trends and raise the bar on proficiency, skills, and competencies to meet client demand.”",
   },
 ];
+
+const Community: React.FC = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handlePrevClick = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? data.length - 1 : prevIndex - 1
+    );
+  };
+
+  const handleNextClick = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === data.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  return (
+    <div className="communityhead">
+      <div className="community">
+        <p>From the Faculte community</p>
+
+        <div className="community__body-container">
+          <div
+            className="community__body"
+            style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+          >
+            {data.map((d, index) => (
+              <div className="community__body1" key={index}>
+                <img src={d.img} alt={d.name} />
+                <div className="community__body1content">
+                  <p>{d.name}</p>
+                  <p className="text">{d.text}</p>
+                  <p>{d.review}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div className="buttons">
+          <button onClick={handlePrevClick}>
+            <FaRegArrowAltCircleLeft />
+          </button>
+          <button onClick={handleNextClick}>
+            <FaRegArrowAltCircleRight />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Community;
