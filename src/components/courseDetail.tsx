c5de06e6f7be8f38262953e7fd9d49a2da0fcf9e
Diptech-hub <oladipupobabayemi@gmail.com>
2024-07-24 14:49:54 +0100

    courseDetails update

--- src/components/courseDetail.tsx
+++ src/components/courseDetail.tsx
@@ -3,8 +3,16 @@ import { useDispatch, useSelector } from "react-redux";
 import { useParams, useNavigate } from "react-router-dom";
 import { RootState, AppDispatch } from "../store";
 import { fetchCourseDetails } from "../features/fetch/courseList";
-import { PiGreaterThanBold } from "react-icons/pi";
+import {
+  PiGreaterThanBold,
+  PiShoppingCartSimpleBold,
+  PiExam,
+  PiCertificate,
+} from "react-icons/pi";
 import { FaRegHeart } from "react-icons/fa";
+import { CiYoutube } from "react-icons/ci";
+import { MdOutlineArticle, MdDevices } from "react-icons/md";
+import { FiDownload } from "react-icons/fi";
 import { BiShare } from "react-icons/bi";
 import NavBar3 from "./navBar3";
 import "../styles/courseDetail.css";
@@ -46,54 +54,106 @@ const CourseDetails: React.FC = () => {
           <PiGreaterThanBold /> <span>{selectedCourse.courseTitle}</span>
         </p>
       </div>
-      {/* {loading && <p>Loading...</p>}
-          {error && <p>Error: {error}</p>} */}
-      <div className="courseDetails_body1">
-        <div className="courseDetails_body1Head">
-          <p>{selectedCourse.courseTitle}</p>
-          <p>{selectedCourse.courseType}</p>
-          <div className="courseDetails_body1HeadList">
-            <p>
-              Last Updates <span>{selectedCourse.dateCreated}</span>
-            </p>
-            <p>
-              Level <span>{selectedCourse.courseType}</span>
-            </p>
-            <p>
-              Type <span>{selectedCourse.learningType}</span>
-            </p>
-            <p>
-              Language <span>{selectedCourse.language}</span>
-            </p>
-            <button>
-              <FaRegHeart />
-              Wishlist
-            </button>
-            <button>
-              <BiShare />
-              Share
-            </button>
+      <div className="courseDetails_body">
+        <div className="courseDetails_body1">
+          <div className="courseDetails_body1Head">
+            <p>{selectedCourse.courseTitle}</p>
+            <p>{selectedCourse.courseType}</p>
+            <div className="courseDetails_body1HeadList">
+              <p>
+                Last Updates <span>{selectedCourse.dateCreated}</span>
+              </p>
+              <p>
+                Level <span>{selectedCourse.courseType}</span>
+              </p>
+              <p>
+                Type <span>{selectedCourse.learningType}</span>
+              </p>
+              <p>
+                Language <span>{selectedCourse.language}</span>
+              </p>
+              <button>
+                <FaRegHeart />
+                Wishlist
+              </button>
+              <button>
+                <BiShare />
+                Share
+              </button>
+            </div>
+            <video width="600" controls>
+              <source src={selectedCourse.courseVideo} type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
+          </div>
+          <div className="courseDetails_overview">
+            <p>Overview</p>
+            <p>{selectedCourse.markdown}</p>
+          </div>
+          <div className="courseDetails_learn">
+            <p>What you'll learn </p>
+            <ul>
+              <li>Introduction to the course in details</li>
+              <li>Obtain strong knowledge of the course and use cases</li>
+              <li>Write your own independent program</li>
+              <li>Understand in details</li>
+              <li>Indepth knowledge</li>
+            </ul>
+          </div>
+          <div className="courseDetails_require">
+            <p>Requirements</p>
+            <ul>
+              <li>Access to a computer system</li>
+              <li>Internet connection to setup development and watch videos</li>
+            </ul>
           </div>
         </div>
-        <img
-          src={selectedCourse.courseImage}
-          alt={selectedCourse.courseTitle}
-        />
-        <p>Level: {selectedCourse.courseLevel}</p>
-        <p>Learning Type: {selectedCourse.learningType}</p>
-        <p>Discount Price: #{selectedCourse.discountPrice}</p>
-        <p>Actual Price: #{selectedCourse.actualPrice}</p>
-        <p>Branch: {selectedCourse.courseBranch}</p>
-        <p>Type: {selectedCourse.courseType}</p>
-        <p>Date Created: {selectedCourse.dateCreated}</p>
-        <p>Language: {selectedCourse.language}</p>
-        <video width="600" controls>
-          <source src={selectedCourse.courseVideo} type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-        <div
-          dangerouslySetInnerHTML={{ __html: selectedCourse.markdown }}
-        ></div>
+        <div className="courseDetails_body2">
+          <div className="coursePrice">
+            <p>{selectedCourse.discountPrice}</p>
+            <p>
+              {selectedCourse.actualPrice}
+              <span>40% off</span>
+            </p>
+            <button>
+              <PiShoppingCartSimpleBold />
+              Enroll
+            </button>
+          </div>
+          <div className="courseLists">
+            <p>This course includes</p>
+            <ul>
+              <li>
+                <CiYoutube />
+                Months of lecture
+              </li>
+              <li>
+                <MdOutlineArticle />
+                Articles
+              </li>
+              <li>
+                <FiDownload />
+                Downloadable resources
+              </li>
+              <li>
+                <PiExam />
+                Tests
+              </li>
+              <li>
+                <PiExam />
+                Exams
+              </li>
+              <li>
+                <MdDevices />
+                Access to Mobile and TV
+              </li>
+              <li>
+                <PiCertificate />
+                Certificate of completion
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
