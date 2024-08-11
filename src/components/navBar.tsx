fe7cbbc3177f09b131b2b29efe126e7cd1eec968
Diptech-hub <oladipupobabayemi@gmail.com>
2024-08-11 20:48:46 +0100

    styling and responsiveness

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -1,21 +1,55 @@
+import { useState } from "react";
 import { LuShoppingCart } from "react-icons/lu";
-import { Link } from "react-router-dom"
-import "../styles/navBar.css"
+import { RxHamburgerMenu } from "react-icons/rx";
+import { IoMdClose } from "react-icons/io";
+import { Link } from "react-router-dom";
+import "../styles/navBar.css";
 
 const NavBar: React.FC = () => {
-    return (
-      <div className="topBar">
-        <p className="top-name">Faculte</p>
-        <div className="body">
-            <a href="">Categories</a>
-            <a href="">For Schools</a>
-            <a href="">Teach</a>
-            <a href=""><LuShoppingCart /></a>
-            <Link to="/login"><a href="">Login</a></Link>
-            <Link to="/signup"><button className="signup">Signup</button></Link>
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <div className="topBar">
+      <p className="top-name">Faculte</p>
+      <button className="mobileBtn" onClick={toggleMenu}>
+        {isMenuOpen ? <IoMdClose /> : <RxHamburgerMenu />}
+      </button>
+      {isMenuOpen && (
+        <div className="mobileBody">
+          <a href="">Categories</a>
+          <a href="">For Schools</a>
+          <a href="">Teach</a>
+          <a href="">
+            <LuShoppingCart />
+          </a>
+          <Link to="/login">
+            <a href="">Login</a>
+          </Link>
+          <Link to="/signup">
+            <button className="signup">Signup</button>
+          </Link>
         </div>
+      )}
+      <div className="body">
+        <a href="">Categories</a>
+        <a href="">For Schools</a>
+        <a href="">Teach</a>
+        <a href="">
+          <LuShoppingCart />
+        </a>
+        <Link to="/login">
+          <a href="">Login</a>
+        </Link>
+        <Link to="/signup">
+          <button className="signup">Signup</button>
+        </Link>
       </div>
-    );
-  }
-  
-  export default NavBar;
+    </div>
+  );
+};
+
+export default NavBar;
